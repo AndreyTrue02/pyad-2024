5ddfae6dfa94163c4e65039036e773a898114bbe
AndreyTrue02 <73782641+AndreyTrue02@users.noreply.github.com>
2025-02-20 14:34:36 +0300

    Homework 02 Трубников Андрей
    https://colab.research.google.com/drive/1g2vhzBjCeYB4105AjspAAw1W44wNKV-o?usp=sharing

--- lab2/personal_recommendation.py
+++ lab2/personal_recommendation.py
@@ -1,1 +1,2 @@
 
+# Рекомендованные книги для пользователя 198711: ['Harry Potter and the Chamber of Secrets (Book 2)', "The Lion, the Witch and the Wardrobe (Full-Color Collector's Edition)", "The Magician's Nephew (Narnia)", 'The Talisman', 'Little House in the Big Woods']
